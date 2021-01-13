0188be67af682df1475c2802b4d4ab7f8c1e045e
slimane rechdi <slimane.rechdi@gmail.com>
2021-01-13 10:04:56 -0500

    Add project files.

---
+++ CreatePlate/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open SolidEdgeCommunity.Extensions
+
+(*
+This code create a Part:
+    SheetMetal -> Tab
+*)
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv =
+    let application = SolidEdgeCommunity.SolidEdgeUtils.Connect(true, true)
+    let documents = application.Documents;
+    let sheetMetalDocument = documents.AddSheetMetalDocument()
+    let model = SheetMetalHelper.CreateBaseTab(sheetMetalDocument)
+    let tabs = model.Tabs
+    let tab = tabs.Item(1)
+    let mutable selectSet = application.ActiveSelectSet
+    selectSet.RemoveAll()
+    selectSet.Add(tab)
+    application.StartCommand(SolidEdgeConstants.SheetMetalCommandConstants.SheetMetalViewISOView);
+    0
